5a0b57234dd8524d4dbb712c3591d0d53021b39a
Francisco Durán Aparici <Paco@MacBook-Pro-de-Paco.local>
2020-06-12 19:35:08 +0200

    youtube lazy-load

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 console.log('WikiFront');
 console.log('PEC 1 - Herramientas HTML y CSS');
-console.log('Paco Durán Aparici');       
+console.log('Paco Durán Aparici');
+
+// Lazy Youtube.js
+function setVideoFrames() {
+    window.setTimeout(function () {
+        let youtube_iframes = document.querySelectorAll('.lazy-youtube');
+
+        if (youtube_iframes !== null) {
+            for (let i = 0; i < youtube_iframes.length; i++) {
+                youtube_iframes[i].src = youtube_iframes[i].getAttribute('data-src');
+            }
+        }
+    }, 2000);
+}
+
+if (window.addEventListener)  // W3C DOM
+    window.addEventListener('load', setVideoFrames, false);
+else if (window.attachEvent) { // IE DOM
+    window.attachEvent('onload', setVideoFrames);
+} else { //NO SUPPORT, lauching right now
+    setVideoFrames();
+}
